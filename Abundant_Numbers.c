142dca7d1c3e4573e0c6a15d2b45e9a49f72fb10
varma077 <97425766+varma077@users.noreply.github.com>
2022-10-15 18:50:21 +0530

    #include<stdio.h> int main() {     int num,i;     scanf("%d",&num);     int sum=0;     for( i=1;i<num;i++)     {         if(num%i==0)         {             sum=sum+i;         }     }
       if(sum>num)
        printf("True");
       else
        printf("False");
    }

---
+++ Abundant_Numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    scanf("%d",&num);
+    int sum=0;
+    for( i=1;i<num;i++)
+    {
+        if(num%i==0)
+        {
+            sum=sum+i;
+        }
+    }
+
+   if(sum>num)
+    printf("True");
+   else
+    printf("False");
+}
